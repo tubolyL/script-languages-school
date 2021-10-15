82903dff7578640df3e49b5c4439215dd5175cb6
Levente <levente.tuboly@gmail.com>
2021-10-15 10:28:40 +0200

    orai_munka2

--- 02/warhorse_loot.py
+++ 02/warhorse_loot.py
@@ -1,20 +1,22 @@
 import random
 
 
-
 LOOT_CHANCE = 0.006
 NO_SIMULATIONS = int(1e3)
 
-def hasLoot() -> bool:
+
+def has_loot() -> bool:
     return random.random() < LOOT_CHANCE
 
-def week_sequence(no_of_weeks: int) ->bool:
-    return any([hasLoot() for _ in range(no_of_weeks)])
+
+def week_sequence(no_of_weeks: int) -> bool:
+    return any([has_loot() for _ in range(no_of_weeks)])
+
 
 if __name__ == "__main__":
     for sequence_length in range(1, 12):
         loot_count = 0
         for _ in range(NO_SIMULATIONS):
             if week_sequence(sequence_length):
-                loot_count+=1
+                loot_count += 1
         print(f"{sequence_length}: {round(100*loot_count/NO_SIMULATIONS)}")
